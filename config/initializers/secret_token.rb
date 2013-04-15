77e2fb5b805a7b68825f7c3da9ed9b69f860b7e7
jasoncox <jason@audioandpixels.com>
2013-04-15 12:43:06 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagtipCom::Application.config.secret_token = '6273e4e9d2372135fc84e0440f5846ca7f9ed26df8d0a0efd1680224e71e5617ad238a55ffc0fe10ba434e067645db066c5194b1acc123f1c23c5e3d3e4e1ae8'
